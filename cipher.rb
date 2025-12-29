5066b19d765fa41bd473f8bd2e200b0717f13fa7
BelakJG <74761299+BelakJG@users.noreply.github.com>
2025-12-29 13:56:45 -0500

    Cleaned up code

--- cipher.rb
+++ cipher.rb
@@ -1,33 +1,31 @@
-def caesar_cipher(orig_string, shift_amount = 1) 
-  shifted_string = ""
+# frozen_string_literal: true
+
+def caesar_cipher(orig_string, shift_amount = 1)
+  shifted_string = ''
   orig_string.each_char do |char|
-    if alphabetical?(char)
-      if capital?(char)
-        shifted_string += shift_upper(char, shift_amount)
-      else
-        shifted_string += shift_lower(char, shift_amount)
-      end
-    else
-      shifted_string += char
-    end
+    shifted_string += if alphabetical?(char)
+                        if capital?(char)
+                          shift_upper(char, shift_amount)
+                        else
+                          shift_lower(char, shift_amount)
+                        end
+                      else
+                        char
+                      end
   end
   p shifted_string
 end
 
 def shift_upper(chr, shift_amount)
   shifted_chr = chr.ord + shift_amount
-  if shifted_chr > 90 
-    shifted_chr -= 26
-  end
-  return shifted_chr.chr
+  shifted_chr -= 26 if shifted_chr > 90
+  shifted_chr.chr
 end
 
 def shift_lower(chr, shift_amount)
   shifted_chr = chr.ord + shift_amount
-  if shifted_chr > 122
-    shifted_chr -= 26
-  end
-  return shifted_chr.chr
+  shifted_chr -= 26 if shifted_chr > 122
+  shifted_chr.chr
 end
 
 def capital?(chr)
@@ -35,8 +33,7 @@ def capital?(chr)
 end
 
 def alphabetical?(chr)
-  chr.match("[a-zA-Z]")
+  chr.match('[a-zA-Z]')
 end
 
-
-caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
